8077920d76e52dd5e43a03ccea959dd4cdb9731d
Qi <904199076@qq.com>
2021-02-12 23:22:00 +0800

    version 1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,107 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Upload from 'antd/es/upload';
+import { UploadChangeParam } from 'antd/es/upload/interface';
+import message from 'antd/es/message';
+
+import Paragraph from 'antd/es/typography/Paragraph';
+
+import { InboxOutlined } from '@ant-design/icons';
+import 'antd/dist/antd.css';
+
+import List, { FileData } from './List';
 import './App.css';
 
+interface FileList {
+  type: string;
+  name: string;
+}
+interface tempFileData {
+  [index: string]: FileData;
+}
+
+const { Dragger } = Upload;
+
+const fileListUrl = './list.php';
+const process = (data: string[]): FileData[] => {
+  const fileList: FileList[] = [];
+  data.forEach((item) => {
+    let type = '';
+    if (item.split('.').length > 1) {
+      type = item.split('.').pop() || 'blank';
+    }
+    fileList.push({ type, name: item });
+  });
+  let tmp: tempFileData = {};
+  fileList.forEach((item) => {
+    if (!tmp[item.type]) {
+      tmp[item.type] = {
+        type: item.type,
+        files: [],
+      };
+    }
+    tmp[item.type]['files'].push(item.name);
+  });
+
+  const res: FileData[] = [];
+  Object.keys(tmp).forEach((key) => {
+    res.push(tmp[key]);
+  });
+
+  return res.sort((a, b) => {
+    return b.files.length - a.files.length;
+  });
+};
+
 function App() {
+  const [needRefresh, refresh] = useState(Boolean);
+  const [fileList, setFileList] = useState<FileData[]>([]);
+  useEffect(() => {
+    fetch(fileListUrl).then((res) => {
+      res.json().then((data: string[]) => {
+        setFileList(
+          process(
+            data.filter((name) => {
+              return name !== '.' && name !== '..';
+            })
+          )
+        );
+      });
+    });
+  }, [needRefresh]);
+
+  const props = {
+    name: 'myFile',
+    multiple: true,
+    action: './upload.php',
+    onChange(info: UploadChangeParam) {
+      const { status } = info.file;
+      if (status !== 'uploading') {
+        // console.log(info.file, info.fileList);
+      }
+      if (status === 'done') {
+        // message.success(`${info.file.name} file uploaded successfully.`);
+        refresh(!needRefresh);
+      } else if (status === 'error') {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+    defaultFiLeList: [],
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>文件传递助手</h1>
+      <div className="uploader">
+        <Dragger {...props}>
+          <p className="ant-upload-drag-icon">
+            <InboxOutlined />
+          </p>
+          <p className="ant-upload-text">点击或拖拽文件上传</p>
+        </Dragger>
+      </div>
+      <List data={fileList} />
+      <Paragraph>
+        还在使用微信文件传输助手？通过容器方式，快速部署可信网络下的文件传输，实现手机笔记本同子网络，文件交换，同时支持可靠的速度传输。
+      </Paragraph>
     </div>
   );
 }
---
+++ src/List.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import List from 'antd/es/list';
+import Tag from 'antd/es/tag';
+
+
+export interface FileData {
+  type: string,
+  files: string[]
+};
+
+
+
+const genDarkColor = (): string => {
+  const color = Math.random().toString(16).slice(2, 8);
+  let res = [];
+  for (let i = 0; i < color.length; i += 2) {
+    const n = color.split('').splice(i, 2).join('');
+    res.push(parseInt(n, 16));
+  }
+  const [r, g, b] = res;
+  if (r * 0.299 + g * 0.578 + b * 0.114 >= 100) {
+    // 越小颜色越深
+    // console.log('re gen...');
+    return genDarkColor();
+  } else {
+    return color;
+  }
+};
+
+const TagHeader = (props: { tag: string }) => {
+  return <Tag color={`#${genDarkColor()}`}>{props.tag}</Tag>;
+};
+
+export default function FileList(props: {data: FileData[]}) {
+  const data: FileData[] = props.data;
+  return (
+    <div className="list">
+      <List
+        grid={{
+          gutter: 64,
+          xs: 1,
+          sm: 2,
+          md: 3,
+          lg: 4,
+          xl: 4,
+          xxl: 6,
+        }}
+        dataSource={data}
+        renderItem={(item) => (
+          <List.Item>
+            <List
+              size="small"
+              header={<TagHeader tag={item.type} />}
+              dataSource={item.files}
+              renderItem={(file) => (
+                <List.Item>
+                  <a href={`/uploads/${file}`}>{file}</a>
+                </List.Item>
+              )}
+            />
+          </List.Item>
+        )}
+      />
+    </div>
+  );
+}
